6347ae12075de2d9249c80ad184a3bae66b40745
GiulioSnider <giuliosnider@gmail.com>
2022-10-20 16:43:28 +0200

    Mezza via nella Service

--- APITrattori/Services/Worker/TrattoriWorkerService.cs
+++ APITrattori/Services/Worker/TrattoriWorkerService.cs
@@ -1,33 +1,53 @@
 ﻿using APITrattori.Models;
+using APITrattori.Services.DA.Interface;
 using APITrattori.Services.Worker.Interfaces;
+using System.Drawing;
 
 namespace APITrattori.Services.Worker
 {
     public class TrattoriWorkerService : ITrattoriWorkerService
     {
-        public void DeleteById(int idTrattore)
+        private readonly IDATrattoriFile _DATrattoriFile;
+
+        public TrattoriWorkerService(IDATrattoriFile dATrattoriFile)
         {
-            throw new NotImplementedException();
+            _DATrattoriFile = dATrattoriFile;
+        }
+
+        public Trattore Post(PostTrattore postTrattoreModel)
+        {
+            Trattore fullTrattore = new()
+            {
+                TrattoreId = GenerateId(),
+                Marca = postTrattoreModel.Marca,
+                Modello = postTrattoreModel.Modello,
+                Color = postTrattoreModel.Color
+            };
+            _DATrattoriFile.AddSingleTrattore(fullTrattore);
+            return fullTrattore;
+        }
+
+        public Trattore GetById(int idTrattore)
+        {
+            return _DATrattoriFile.GetById(idTrattore);
         }
 
         public List<Trattore> GetAllByColors(Colore colore)
         {
             throw new NotImplementedException();
         }
-
-        public Trattore GetById(int idTrattore)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Trattore Post(PostTrattore postTrattoreModel)
-        {
-            throw new NotImplementedException();
-        }
-
         public Trattore Put(PostTrattore postTrattoreModel, int idTrattore)
         {
             throw new NotImplementedException();
+        }
+
+        public void DeleteById(int idTrattore)
+        {
+            throw new NotImplementedException();
+        }
+        private int GenerateId()
+        {
+            return _DATrattoriFile.GetAll().Max(trattore => trattore.TrattoreId) + 1;
         }
     }
 }
